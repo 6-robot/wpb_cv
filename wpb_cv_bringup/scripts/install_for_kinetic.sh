5bae9c6f1e6a06ef879db1daa1b12dfd66838046
zhangwanjie <zhangwanjie@126.com>
2020-10-27 19:15:50 +0800

    add scripts

---
+++ wpb_cv_bringup/scripts/install_for_kinetic.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get install ros-kinetic-joy
+sudo apt-get install ros-kinetic-hector-mapping
+sudo apt-get install ros-kinetic-gmapping
+sudo apt-get install ros-kinetic-navigation
+sudo apt-get install ros-kinetic-sound-play
+sudo apt-get install ros-kinetic-astra-camera
+sudo apt-get install ros-kinetic-astra-launch
+sudo apt-get install ros-kinetic-depthimage-to-laserscan
---
+++ wpb_cv_bringup/scripts/install_for_melodic.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get install ros-melodic-joy
+sudo apt-get install ros-melodic-hector-mapping
+sudo apt-get install ros-melodic-gmapping
+sudo apt-get install ros-melodic-navigation
+sudo apt-get install ros-melodic-sound-play
+sudo apt-get install ros-melodic-astra-camera
+sudo apt-get install ros-melodic-astra-launch
+sudo apt-get install ros-melodic-depthimage-to-laserscan
